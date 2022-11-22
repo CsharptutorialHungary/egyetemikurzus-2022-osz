107c4d2b6887f9dc2b516b5a873b383342529af5
valti01 <valti0190@gmail.com>
2022-11-22 22:29:10 +0100

    alapok létrehozása (player class, player nevének megadása)

---
+++ FMEQVW_Roulette/FMEQVW_Roulette/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Player
+{
+	public string name { get; set; }
+	public int currency { get; set; }
+
+	public Player()
+	{
+		name = string.Empty;
+		currency = 0;
+	}
+
+	public Player(string name, int currency)
+	{
+		this.name = name;
+		this.currency=currency;
+	}
+}
---
+++ FMEQVW_Roulette/FMEQVW_Roulette/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Add meg a játékos nevét:");
+Player player = new Player();
+player.name = "aa";
+Console.WriteLine(player.name);
+Console.ReadKey();
