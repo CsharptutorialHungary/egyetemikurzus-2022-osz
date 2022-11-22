65243cf48e4acd95b471185a232f28745f693517
valti01 <valti0190@gmail.com>
2022-11-22 22:35:22 +0100

    player nevének beolvasása konzolról, kezdetleges game loop

--- FMEQVW_Roulette/FMEQVW_Roulette/Program.cs
+++ FMEQVW_Roulette/FMEQVW_Roulette/Program.cs
@@ -1,5 +1,12 @@
 ﻿Console.WriteLine("Add meg a játékos nevét:");
 Player player = new Player();
-player.name = "aa";
+player.name = Console.ReadLine();
 Console.WriteLine(player.name);
+string input = string.Empty;
+while (input!="Q"){
+    Console.WriteLine("Exit command: Q");
+    //TODO: game loop
+    input = Console.ReadLine();
+}
+
 Console.ReadKey();
