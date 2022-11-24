ede8054bccc2482ce3ba1620eb833d29b7b946ec
Czékus Miklós <h153430@stud.u-szeged.hu>
2022-11-24 11:52:56 +0100

    Command interface created

---
+++ OQQA67/IBlackJackCommands.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OQQA67
+{
+    internal interface IBlackJackCommands
+    {
+        string Name { get; }
+        void Execute();
+    }
+}
---
+++ OQQA67/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OQQA67
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
