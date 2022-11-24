b11dc011bea6bca6cdde29aba25b17aac0bfcb66
Czékus Miklós <h153430@stud.u-szeged.hu>
2022-11-24 20:04:30 +0100

    UserLoaderSaver created

---
+++ OQQA67/UserLoaderSaver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace OQQA67
+{
+    internal static class UserLoaderSaver
+    {
+
+        
+        
+    }
+}
