c1b70620b53dd10a699c5fa4b7a32ec3fdfb14b3
Juhász Csaba József <h870697@stud.u-szeged.hu>
2022-12-04 17:33:31 +0100

    Add basic console functionality to main function, not complete

--- SSRCDT/Program.cs
+++ SSRCDT/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SSRCDT
 {
@@ -12,6 +8,41 @@ namespace SSRCDT
         {
             Console.WriteLine("hello");
             System.Threading.Thread.Sleep(1000);
+            Fryer fryer1 = new Fryer(false);
+            Fryer fryer2 = new Fryer(false);
+            Fryer fryer3 = new Fryer(false);
+            Fryer fryer_kent = new Fryer(true);
+
+            string input = "";
+
+            while(input != "q")
+            {
+                Console.WriteLine("A kilepeshez nyomj q-t");
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "fry":
+                        {
+                            Console.WriteLine("Milyen húst szeretnél sütni? (StripsMeat)");
+                            string meatType = Console.ReadLine();
+                            Console.WriteLine("Hány darabot sütsz? (szám)");
+                            bool isParsable = int.TryParse(Console.ReadLine(), out int meatCount);
+                            if (isParsable)
+                            {
+                                Console.WriteLine(meatType);
+                                Console.WriteLine(meatCount);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Hiba: Nem számot adtál meg!");
+                            }
+                            break;
+                        }
+                    default: break;
+                }
+            }
+
         }
+
     }
 }
