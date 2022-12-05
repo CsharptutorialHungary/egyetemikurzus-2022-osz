4a4e9ed3bc2d09a4035bc32ba6320b710c47c27a
Juhász Csaba József <h870697@stud.u-szeged.hu>
2022-12-05 23:41:00 +0100

    Add basic MeatHolder class

---
+++ SSRCDT/MeatHolder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSRCDT
+{
+    internal class MeatHolder
+    {
+        public List<Meat> Container;
+        public MeatHolder() 
+        {
+            this.Container = new List<Meat>();
+        }
+
+        public void AddToContainer(Meat meat)
+        {
+            this.Container.Append(meat);
+        }
+    }
+}
