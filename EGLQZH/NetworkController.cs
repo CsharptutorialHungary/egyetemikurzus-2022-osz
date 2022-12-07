ca8dabd525690b25f7b8aedea4164f1a12fe2bd1
Guczi Ádám <gucziadamlaszlo@gmail.com>
2022-12-07 02:34:58 +0100

    Networking refactor

--- EGLQZH/NetworkController.cs
+++ EGLQZH/NetworkController.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Diagnostics;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -8,19 +9,25 @@ namespace TicTacToe {
         static readonly CancellationTokenSource cts = new();
 
         public static async Task<List<IPAddress>> Discover() {
-            await SendBroadcast();
+            await SendMessage("DISCOVER");
 
             return await ListenToDiscover();
         }
 
         private static async Task<List<IPAddress>> ListenToDiscover() {
-            
+            IPAddress localIP = LocalIPAddress();
 
             List<IPAddress> addresses = new();
             var listen = Task.Run(async () => {
                 while (!cts.IsCancellationRequested) {
-                    var recv = await udpClient.ReceiveAsync(cts.Token);
-                    addresses.Add(recv.RemoteEndPoint.Address);
+                    var (message, remoteAddress) = await ReceiveMessage();
+                    //Trace.WriteLine($"Received message from {remoteAddress}: '{message}'");
+                    if (!addresses.Contains(remoteAddress)) {
+                        addresses.Add(remoteAddress);
+
+                        await SendMessage("ACK", remoteAddress);
+                        GameController.renderer.DrawDiscover(addresses);
+                    }
                 }
             });
 
@@ -33,25 +40,33 @@ namespace TicTacToe {
             return addresses;
         }
 
-        private static async Task<int> SendBroadcast() {
-            udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, port));
+        private static async Task<(string, IPAddress)> ReceiveMessage() {
+            var recv = await udpClient.ReceiveAsync(cts.Token);
+            var recvMessage = Encoding.UTF8.GetString(recv.Buffer);
+            IPAddress recvAddress = recv.RemoteEndPoint.Address;
 
-            var discoverMessageText = "DISCOVER";
-            var discoverMessage = Encoding.UTF8.GetBytes(discoverMessageText);
+            return (recvMessage, recvAddress);
+        }
+
+        private static async Task<int> SendMessage(string msg, IPAddress? to = null) {
+            udpClient.Client.Bind(new IPEndPoint(to ?? IPAddress.Any, port));
+
+            var discoverMessage = Encoding.UTF8.GetBytes(msg);
             // TODO: Get network's broadcast address
             return await udpClient.SendAsync(discoverMessage, discoverMessage.Length, "255.255.255.255", port);
         }
 
-        private static IPAddress? LocalIPAddress() {
+        private static IPAddress LocalIPAddress() {
             if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable()) {
-                return null;
+                GameController.renderer.DrawError("Please connect to a network before playing!");
+                Environment.Exit(1);
             }
 
             IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
 
             return host
                 .AddressList
-                .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+                .First(ip => ip.AddressFamily == AddressFamily.InterNetwork);
         }
         private const ushort port = 32555;
     }
